cefa339de1d1632dc7b184b260e38c5638d883f7
Andrew Palmer <atp@sdf.org>
2021-02-25 23:41:14 -0600

    Test lexing complex input

--- main.c
+++ main.c
@@ -3,14 +3,23 @@
 #include "lexer.h"
 
 void test_lexer() {
-    Lexer *lexer = lexer_new("123 + 456");
+    static const char *PROGRAM =
+        "x = 2.0\n"
+        "x * 1.5 + (123 - 456)\n"
+        ;
 
-    printf("data: %s\n", lexer->data);
+    Lexer *lexer = lexer_new(PROGRAM);
+
+    printf("PROGRAM:\n%s\n", lexer->data);
 
     while(lexer_next(lexer)) {
         Token *token = lexer_curr(lexer);
-        if(token->type == TOKT_NUMBER)
+        if(token->type == TOKT_END)
+            break;
+        else if(token->type == TOKT_NUMBER)
             printf("number: %f\n", ((NumericToken *)token)->value);
+        else if(token->type == TOKT_VARIABLE)
+            printf("variable: \"%s\"\n", ((VarNameToken *)token)->value);
         else
             printf("Type: %c (%d)\n", token->type, token->type);
     }
