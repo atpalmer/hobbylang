9806fc16cc45e08b4c79939c2cf0c0ecd5275672
Andrew Palmer <atp@sdf.org>
2021-03-01 13:50:56 -0600

    Implement one-line calc

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
 
+struct line {
+    size_t size;
+    char *buff;
+};
 
 int main(void) {
+    struct line line = {
+        .size = 128,
+        .buff = malloc(sizeof(char) * 128),
+    };
+    printf(">>> ");
+    getline(&line.buff, &line.size, stdin);
+
+    Parser *parser = parser_new(line.buff);
+    double value = parser_next_line(parser);
+    printf("%f\n", value);
+    parser_free(parser);
+
+    free(line.buff);
 }
