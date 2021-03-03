9102d092e4d80e8a5e7717975ed2fdd3b79022de
Andrew Palmer <atp@sdf.org>
2021-03-02 18:10:41 -0600

    Move token check

--- error.h
+++ error.h
@@ -2,12 +2,21 @@
 #define ERROR_H
 
 #include <errno.h>
+#include "lexer.h"
 
 static inline void error_ensure_errno_ok(void) {
     if(!errno)
         return;
     perror("System Error");
     exit(errno);
+}
+
+static inline void error_ensure_token_type(Token *token, TokenType expected) {
+    if(token_type(token) == expected)
+        return;
+    fprintf(stderr, "TokenType Error: Expected: %d. Received: %d\n",
+        expected, token_type(token));
+    exit(-1);
 }
 
 #endif
