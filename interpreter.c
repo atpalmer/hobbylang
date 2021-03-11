14ce856b4ab3d81c4c60ba74fc3e69bc938c13a6
Andrew Palmer <atp@sdf.org>
2021-03-11 00:52:38 -0600

    Pass vars to interpreter

--- interpreter.c
+++ interpreter.c
@@ -2,55 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ast.h"
+#include "varmap.h"
 #include "interpreter.h"
 
-double _interpret_ast(AstNode *ast) {
+double _interpret_ast(AstNode *ast, VarEntry *vars) {
     if(ast->type == ASTT_DOUBLE) {
         return ((AstDoubleNode *)ast)->value;
     }
     if(ast->type == ASTT_BINOP) {
         AstBinOpNode *node = (AstBinOpNode *)ast;
         if(node->op == ASTOP_EQ) {
-            return _interpret_ast(node->left) == _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) == _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_NE) {
-            return _interpret_ast(node->left) != _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) != _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_LT) {
-            return _interpret_ast(node->left) < _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) < _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_GT) {
-            return _interpret_ast(node->left) > _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) > _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_PLUS) {
-            return _interpret_ast(node->left) + _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) + _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_MINUS) {
-            return _interpret_ast(node->left) - _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) - _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_MULT) {
-            return _interpret_ast(node->left) * _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) * _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_DIV) {
-            return _interpret_ast(node->left) / _interpret_ast(node->right);
+            return _interpret_ast(node->left, vars) / _interpret_ast(node->right, vars);
         }
         if(node->op == ASTOP_FLOORDIV) {
-            return floor(_interpret_ast(node->left) / _interpret_ast(node->right));
+            return floor(_interpret_ast(node->left, vars) / _interpret_ast(node->right, vars));
         }
         if(node->op == ASTOP_MOD) {
-            return fmod(_interpret_ast(node->left), _interpret_ast(node->right));
+            return fmod(_interpret_ast(node->left, vars), _interpret_ast(node->right, vars));
         }
         if(node->op == ASTOP_POW) {
-            return pow(_interpret_ast(node->left), _interpret_ast(node->right));
+            return pow(_interpret_ast(node->left, vars), _interpret_ast(node->right, vars));
         }
     }
     if(ast->type == ASTT_UOP) {
         AstUnaryOpNode *node = (AstUnaryOpNode *)ast;
         if(node->op == ASTOP_UPLUS) {
-            return +_interpret_ast(node->operand);
+            return +_interpret_ast(node->operand, vars);
         }
         if(node->op == ASTOP_UMINUS) {
-            return -_interpret_ast(node->operand);
+            return -_interpret_ast(node->operand, vars);
         }
     }
 
@@ -60,7 +61,7 @@ double _interpret_ast(AstNode *ast) {
 
 double interpreter_invoke(Parser *parser) {
     AstNode *ast = parser_line(parser);
-    double result = _interpret_ast(ast);
+    double result = _interpret_ast(ast, parser->varmap);
     ast_free(ast);
     return result;
 }
