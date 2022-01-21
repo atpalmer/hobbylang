46522e668d95b68e7b8cf404c9558db20634116f
Andrew Palmer <atp@sdf.org>
2022-01-21 04:20:01 -0600

    Improve object.h error handling

--- object.h
+++ object.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include "optype.h"
+#include "error.h"
 
 typedef struct object Object;
 
@@ -24,67 +25,59 @@ struct object {
 
 #define OBJECT_INIT(ptr)    ((Object){.as_object = ptr})
 
+static inline const ObjectInterface *Object_as_object(Object *this) {
+    if(!this)
+        die_f(NullReferenceError, "Cannot call %s(NULL)\n", __func__);
+    if(!this->as_object)
+        die(NullReferenceError, "NULL ObjectInterface\n");
+    return this->as_object;
+}
+
 static inline int Object_println(Object *this) {
-    if(!this->as_object->to_stream) {
-        fprintf(stderr, "Error: %s.println\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->to_stream)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->to_stream(this, stdout, "\n");
 }
 
 static inline Object *Object_clone(Object *this) {
-    if(!this->as_object->clone) {
-        fprintf(stderr, "Error: %s.clone\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->clone)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->clone(this);
 }
 
 static inline void Object_destroy(Object *this) {
-    if(!this->as_object->destroy) {
-        fprintf(stderr, "Error: %s.destroy\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->destroy)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     this->as_object->destroy(this);
 }
 
 static inline double Object_as_double(Object *this) {
-    if(!this->as_object->as_double) {
-        fprintf(stderr, "Error: %s.as_double\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->as_double)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->as_double(this);
 }
 
 static inline Object *Object_get(Object *this, const char *key) {
-    if(!this->as_object->get) {
-        fprintf(stderr, "Error: %s.get\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->get)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->get(this, key);
 }
 
 static inline void Object_set(Object *this, const char *key, Object *value) {
-    if(!this->as_object->set) {
-        fprintf(stderr, "Error: %s.set\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->set)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     this->as_object->set(this, key, value);
 }
 
 static inline Object *Object_binop(Object *this, Object *other, BinaryOp op) {
-    if(!this->as_object->binop) {
-        fprintf(stderr, "Error: %s.binop\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->binop)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->binop(this, other, op);
 }
 
 static inline Object *Object_uop(Object *this, UnaryOp op) {
-    if(!this->as_object->uop) {
-        fprintf(stderr, "Error: %s.uop\n", this->as_object->name);
-        exit(-1);
-    }
+    if(!Object_as_object(this)->uop)
+        die_f(NullReferenceError, "%s::%s\n", this->as_object->name, __func__);
     return this->as_object->uop(this, op);
 }
 
